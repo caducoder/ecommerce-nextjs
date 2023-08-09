14d013c12d79e44a0d5f39cb15b58d010296d163
caducoder <cadudrop@hotmail.com>
2023-08-08 22:48:23 -0300

    fix: _app type missing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from 'next/app'
 import { CartProvider } from '../context/CartContext'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
       <Component {...pageProps} />
